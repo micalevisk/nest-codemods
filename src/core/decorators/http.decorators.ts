ac44fa29d8104bf9783362fdcc4ed580b5860241
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2023-02-12 00:21:42 -0400

    refactoring

--- src/core/decorators/http.decorators.ts
+++ src/core/decorators/http.decorators.ts
@@ -1,20 +1,20 @@
 import { Controller } from '@nestjs/common';
 
+/** Removes leading slashes ('/') from `path` */
+const trimPath = (path: string): string => path.replace(/^\/+/, '')
+
 /** Tentant bound controller */
 export function TController(): ClassDecorator;
 export function TController(prefix: string | string[]): ClassDecorator;
 export function TController(maybePrefix?: string | string[]) {
-  /** Removes leading slashes ('/') from `path` */
-  const trimPath = (path: string): string => path.replace(/^\/+/, '')
-
   const PREFIX = 'orgs/:organization_id/';
 
-  if (typeof maybePrefix === 'undefined') maybePrefix = '/';
+  if (typeof maybePrefix === 'undefined') maybePrefix = ['/'];
 
   if (!Array.isArray(maybePrefix)) maybePrefix = [maybePrefix];
 
   maybePrefix.push(
-    ...maybePrefix.map(path => {
+    ...maybePrefix.map((path) => {
       const pathWithoutLeadingSlash = trimPath(path);
       if (pathWithoutLeadingSlash.startsWith(PREFIX)) return path;
       return (PREFIX).concat(pathWithoutLeadingSlash);
